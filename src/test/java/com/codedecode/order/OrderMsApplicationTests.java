48d577f1db938528ccdd8ba4fad82b181abca182
codedecode25 <codergaming25@gmail.com>
2023-07-11 01:40:38 +0530

    test cases and pom modifications

--- src/test/java/com/codedecode/order/OrderMsApplicationTests.java
+++ src/test/java/com/codedecode/order/OrderMsApplicationTests.java
@@ -1,3 +1,4 @@
+/*
 package com.codedecode.order;
 
 import org.junit.jupiter.api.Test;
@@ -11,3 +12,4 @@ class OrderMsApplicationTests {
 	}
 
 }
+*/
---
+++ src/test/java/com/codedecode/order/service/OrderServiceTest.java
@@ -0,0 +1,59 @@
+package com.codedecode.order.service;
+
+import com.codedecode.order.dto.OrderDTO;
+import com.codedecode.order.dto.OrderDTOFromFE;
+import com.codedecode.order.dto.UserDTO;
+import com.codedecode.order.entity.Order;
+import com.codedecode.order.mapper.OrderMapper;
+import com.codedecode.order.repo.OrderRepo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class OrderServiceTest {
+
+    @Mock
+    private OrderRepo orderRepo;
+
+    @Mock
+    private SequenceGenerator sequenceGenerator;
+
+    @Mock
+    private RestTemplate restTemplate;
+
+    @InjectMocks
+    private OrderService orderService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void saveOrderInDb_shouldSaveOrderAndReturnOrderDTO() {
+        // Arrange
+        OrderDTOFromFE orderDetails = new OrderDTOFromFE();
+        Integer newOrderId = 1;
+        UserDTO userDTO = new UserDTO();
+        Order orderToBeSaved = new Order(newOrderId, orderDetails.getFoodItemsList(), orderDetails.getRestaurant(), userDTO);
+        OrderDTO orderDTOExpected = OrderMapper.INSTANCE.mapOrderToOrderDTO(orderToBeSaved);
+
+        when(sequenceGenerator.generateNextOrderId()).thenReturn(newOrderId);
+        when(restTemplate.getForObject(anyString(), eq(UserDTO.class))).thenReturn(userDTO);
+        when(orderRepo.save(orderToBeSaved)).thenReturn(orderToBeSaved);
+
+        // Act
+        OrderDTO orderDTOActual = orderService.saveOrderInDb(orderDetails);
+
+        // Assert
+        verify(sequenceGenerator, times(1)).generateNextOrderId();
+        assertDoesNotThrow(() -> orderService.saveOrderInDb(orderDetails));
+    }
+}
